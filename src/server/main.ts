43ee289cd429721e80c90791bb73a4ced8e318d9
Valentin Radu <radu.v.valentin@me.com>
2018-04-21 12:58:33 +0300

    Added linter. Added Apollo

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,8 +2,6 @@ import * as express from 'express'
 
 const server = express()
 
-console.log('aaa')
 server.listen(process.env.PORT, (err: Error) => {
-  if (err) throw err
-  console.log('> Ready on http://localhost: 3000')
+  if (err) { throw err }
 })
